7364c9e46724b6c4e08e4b436543d88d8b3de000
Harold-Ramirez <ramirezpereiraharold@gmail.com>
2025-01-10 22:34:36 -0400

    refactor: :recycle: Improved scroll section and installed linear-gradient

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import "./global.css";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import Main from "./components/Main";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <View className="flex-1 bg-blue-300">
+      <View className="flex-1">
+        <LinearGradient
+          colors={["#6726b5", "#808080"]}
+          className="absolute top-0 left-0 w-full h-full"
+        />
         <StatusBar style="auto" />
         <Main />
       </View>
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,11 +1,79 @@
 import "../global.css";
-import { Text, View, Image, Pressable, ScrollView } from "react-native";
+import { Text, View, Image, Pressable, FlatList } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
 
 import logo from "../assets/Splash_Logo.png";
 
 export default function Main() {
   const insets = useSafeAreaInsets();
+  const objects = [
+    {
+      name: "Casa",
+      icon: "home",
+    },
+    {
+      name: "Carro",
+      icon: "car",
+    },
+    {
+      name: "Salud",
+      icon: "heart",
+    },
+    {
+      name: "Educación",
+      icon: "graduation-cap",
+    },
+    {
+      name: "Entretenimiento",
+      icon: "gamepad",
+    },
+    {
+      name: "Viajes",
+      icon: "plane",
+    },
+    {
+      name: "Viajes2",
+      icon: "plane",
+    },
+    {
+      name: "Viajes3",
+      icon: "plane",
+    },
+    {
+      name: "Viajes4",
+      icon: "plane",
+    },
+    {
+      name: "Viajes5",
+      icon: "plane",
+    },
+    {
+      name: "Viajes6",
+      icon: "plane",
+    },
+    {
+      name: "Viajes7",
+      icon: "plane",
+    },
+    {
+      name: "Viajes8",
+      icon: "plane",
+    },
+  ];
+
+  function renderItem({ item }) {
+    return (
+      <View className="flex-row h-32 items-center my-2 border-white border-2">
+        <View
+          className={`w-20 h-10 border-green-700 border-2 items-center justify-center rounded-full`}
+        >
+          <Text className="text-white text-md">{item.icon}</Text>
+        </View>
+        <Text className="text-xl text-white ml-2">{item.name}</Text>
+      </View>
+    );
+  }
 
   return (
     <View
@@ -13,41 +81,45 @@ export default function Main() {
         paddingTop: insets.top,
         paddingBottom: insets.bottom,
       }}
+      className="px-3 flex-1"
     >
-      <ScrollView className="px-2">
-        <View className="items-center">
-          <Text className="font-bold text-2xl text-white py-5">
-            BIENVENIDO A TU APP DE AHORRO
-          </Text>
-          <Image
-            source={logo}
-            style={{ width: 100, height: 100 }}
-            className="m-5"
-          />
-          <Pressable className="bg-green-700 border-white border-4 w-48 h-48 my-2 rounded-full justify-center items-center">
-            <Text className="text-white text-2xl">REGISTRAR</Text>
-            <Text className="text-white text-2xl">AHORRO</Text>
-            <Text className="text-white text-2xl">DIARIO</Text>
-          </Pressable>
-        </View>
-        <Text className="my-5 font-bold text-2xl">Tus avances:</Text>
-        <Text className="text-xl">Días con ahorros registrados: 27/30</Text>
-        <Text className="text-xl">
-          Ya tienes un total de XX.xx Bs. ahorrados
+      <View className="items-center">
+        <Text className="font-bold text-2xl text-white py-5">
+          BIENVENIDO A TU APP DE AHORRO
         </Text>
-        <View className="bg-gray-400 w-full h-20 my-2"></View>
-        <View className="bg-gray-400 w-full h-20 my-2"></View>
-        <View className="bg-gray-400 w-full h-20 my-2"></View>
-        <View className="bg-gray-400 w-full h-20 my-2"></View>
-        <View className="bg-gray-400 w-full h-20 my-2"></View>
-        <View className="bg-gray-400 w-full h-20 my-2"></View>
-        <View className="bg-gray-400 w-full h-20 my-2"></View>
-        <View className="bg-gray-400 w-full h-20 my-2"></View>
-        <View className="bg-gray-400 w-full h-20 my-2"></View>
-        <View className="bg-gray-400 w-full h-20 my-2"></View>
-        <View className="bg-gray-400 w-full h-20 my-2"></View>
-        <View className="bg-gray-400 w-full h-20 my-2"></View>
-      </ScrollView>
+        <Image
+          source={logo}
+          style={{ width: 100, height: 100 }}
+          className="m-3"
+        />
+        <Pressable
+          className="bg-green-700 active:bg-green-900 border-white border-4 w-48 h-48 my-2 rounded-full justify-center items-center"
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? "green" : "green",
+            },
+          ]}
+        >
+          <Text className="text-white text-2xl">REGISTRAR</Text>
+          <Text className="text-white text-2xl">AHORRO</Text>
+          <Text className="text-white text-2xl">DIARIO</Text>
+        </Pressable>
+      </View>
+      <Text className="my-2 font-bold text-2xl">Tus avances:</Text>
+      <Text className="text-xl">Días con ahorros registrados: 27/30</Text>
+      <Text className="text-xl mb-2">
+        Ya tienes un total de XX.xx Bs. ahorrados
+      </Text>
+      <LinearGradient
+        colors={["transparent", "black"]}
+        className="w-full h-full flex-1"
+      >
+        <FlatList
+          data={objects}
+          keyExtractor={(item) => item.name}
+          renderItem={renderItem}
+        />
+      </LinearGradient>
     </View>
   );
 }
