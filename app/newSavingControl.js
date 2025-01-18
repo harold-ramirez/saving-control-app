9f4de66e85ea734752bc6ddc078f0f1cda02dd52
Harold-Ramirez <ramirezpereiraharold@gmail.com>
2025-01-17 22:48:01 -0400

    feat: :sparkles: Implemented Tab system and about view

--- app/newSavingControl.js
+++ app/newSavingControl.js
@@ -1,9 +1,22 @@
 import { View, Text } from "react-native";
+import { Screen } from "../components/Screen";
+import { Stack } from "expo-router";
 
 export default function NewSavingControl() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>New Saving Control</Text>
-    </View>
+    <Screen>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: "#ff8f00" },
+          headerTintColor: "white",
+          headerLeft: () => {},
+          headerTitle: "New Saving Control",
+          headerRight: () => {},
+        }}
+      />
+      <View className="flex-1 items-center justify-center">
+        <Text>New Saving Control</Text>
+      </View>
+    </Screen>
   );
 }
--- components/Icons.jsx
+++ components/Icons.jsx
@@ -1,4 +1,5 @@
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 export const PlainIcon = (props) => (
   <FontAwesome6 name="plane" size={24} color="black" {...props} />
@@ -10,4 +11,8 @@ export const MoneyBillIcon = (props) => (
 
 export const CircleInfoIcon = (props) => (
   <FontAwesome6 name="circle-info" size={24} color="white" {...props} />
+);
+
+export const HomeIcon = (props) => (
+  <FontAwesome5 name="home" size={24} color="black" {...props} />
 );
